f4c1ac9a94acd29a8610c3a7fbb0c2761979efdb
mhtess <mtessler@stanford.edu>
2016-05-10 12:39:11 -0700

    transfer script to R; add circle test file

---
+++ demean_age.R
@@ -0,0 +1,8 @@
+setwd("~/Documents/research/misc/reproducibility/testing")
+demographics <- read.csv(file='participants.tsv', head=TRUE, sep="\t")
+age <- demographics[4]
+demean_age <- age - sum(age)/length(age)
+
+write.table(demean_age, file="age_demeaned.tsv", row.names=FALSE, col.names=FALSE, sep="\t")
+
+print("done!")
