2ee2a423b99a6ecf6ea993f2f643211723d53952
mhtess <mtessler@stanford.edu>
2016-05-10 13:02:25 -0700

    add assertion tests

--- demean_age.R
+++ demean_age.R
@@ -1,7 +1,13 @@
 args<-commandArgs(trailingOnly = TRUE)
 demographics <- read.csv(file=args[1], head=TRUE, sep="\t")
 age <- demographics[4]
-demean_age <- age - sum(age)/length(age)
+mean_age <- sum(age) / length(age)
+
+# assertion test
+stopifnot(mean_age < 100)
+stopifnot(mean_age > 10)
+
+demean_age <- age - mean_age
 
 write.table(demean_age, file="age_demeaned.tsv", row.names=FALSE, col.names=FALSE, sep="\t")
 
