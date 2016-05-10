c7e147d6b2093bcd1ba66c9b5f8e6789bd45dacf
mhtess <mtessler@stanford.edu>
2016-05-10 12:54:40 -0700

    change true to TRUE

--- demean_age.R
+++ demean_age.R
@@ -1,4 +1,4 @@
-args<-commandArgs(trailingOnly = true)
+args<-commandArgs(trailingOnly = TRUE)
 demographics <- read.csv(file=args[1], head=TRUE, sep="\t")
 age <- demographics[4]
 demean_age <- age - sum(age)/length(age)
