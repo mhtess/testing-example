759c19e512d261d184ddce60bb3646246c855cfa
mhtess <mtessler@stanford.edu>
2016-05-10 13:05:30 -0700

    replace index with label

--- demean_age.R
+++ demean_age.R
@@ -1,6 +1,7 @@
 args<-commandArgs(trailingOnly = TRUE)
 demographics <- read.csv(file=args[1], head=TRUE, sep="\t")
-age <- demographics[4]
+age <- demographics$age
+
 mean_age <- sum(age) / length(age)
 
 # assertion test
